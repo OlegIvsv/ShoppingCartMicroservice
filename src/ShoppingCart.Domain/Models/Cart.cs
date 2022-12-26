f8bf51495d37ba166ebd5ebec3c1b7aa79378751
Oleh Ivsv <you@example.com>
2022-12-26 21:30:27 +0200

    Add domain models

---
+++ src/ShoppingCart.Domain/Models/Cart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Domain.Models
+{
+    public sealed class Cart
+    {
+        private List<CartItem> _items;
+
+        public string Id { get; private set; }
+        public int CustomerId { get; private set; }
+        public IReadOnlyList<CartItem> Items => _items;
+
+
+        public Cart(string id, int customerId, List<CartItem> items)
+        {
+            Id = id;
+            CustomerId = customerId;
+            _items = items;
+        }
+
+        public Cart(int customerId)
+        {
+            CustomerId = customerId;
+            _items = new List<CartItem>();
+        }
+
+
+        public bool PutItem(CartItem item)
+        {
+            int indexOfItem = _items.FindIndex(i => i.ProductId == item.ProductId);
+            if (indexOfItem >= 0)
+            {
+                _items.RemoveAt(indexOfItem);
+                _items.Insert(indexOfItem, item);
+                return false;
+            }
+            _items.Add(item);
+            return true;
+        }
+
+        public bool RemoveItem(int productId)
+        {
+            int indexOfItem = _items.FindIndex(item => productId == item.ProductId);
+            if (indexOfItem >= 0)
+            {
+                _items.RemoveAt(indexOfItem);
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public bool IsEmpty()
+        {
+            return _items.Count == 0;
+        }
+    }
+}
---
+++ src/ShoppingCart.Domain/Models/CartItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Domain.Models
+{
+    public sealed class CartItem
+    {
+        public int ProductId { get; init; }
+        public string ProductTitle { get; init; }
+        public decimal UnitPrice { get; init; }
+        public int Quantity { get; init; }
+
+        private CartItem(int productId, string productTitle, decimal unitPrice, int quantity)
+        {
+            ProductId = productId;
+            ProductTitle = productTitle;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+        }
+
+        public static CartItem Create(int productId, string productTitle, decimal unitPrice, int quantity)
+        {
+            if (unitPrice <= 0)
+                throw new ValidationException("Invalid unit price!");
+            if (string.IsNullOrWhiteSpace(productTitle))
+                throw new ValidationException("Invalid product title!");
+            if (quantity < 1)
+                throw new ValidationException("Invalid product title!");
+
+            return new(productId, productTitle, unitPrice, quantity);
+        }
+    }
+}
