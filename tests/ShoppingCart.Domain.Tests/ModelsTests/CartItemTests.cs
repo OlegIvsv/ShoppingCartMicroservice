9eb4b85174061eca68e02727565ddd639008d212
Oleh Ivsv <you@example.com>
2022-12-26 21:31:33 +0200

    Add tests for domain models

---
+++ tests/ShoppingCart.Domain.Tests/ModelsTests/CartItemTests.cs
@@ -0,0 +1,42 @@
+﻿using ShoppingCart.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Xunit;
+
+namespace ShoppingCart.Domain.Tests.ModelsTests;
+public class CartItemTests
+{
+    [Theory]
+    [InlineData(0.00)]
+    [InlineData(-1.00)]
+    public void Create_InvalidPrice_ThrowsException(decimal price)
+    {
+        //Act
+        var action = () => CartItem.Create(1, "Test Product", price, 1);
+        //Assert
+        Assert.Throws<ValidationException>(action);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Create_InvalidTitle_ThrowsException(string productTitle)
+    {
+        //Act
+        var action = () => CartItem.Create(1, productTitle, 1.00m, 1);
+        //Assert
+        Assert.Throws<ValidationException>(action);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Create_InvalidQuantity_ThrowsException(int quantity)
+    {
+        //Act
+        var action = () => CartItem.Create(1, "Test Product", 1.00m, quantity);
+        //Assert
+        Assert.Throws<ValidationException>(action);
+    }
+}
+
