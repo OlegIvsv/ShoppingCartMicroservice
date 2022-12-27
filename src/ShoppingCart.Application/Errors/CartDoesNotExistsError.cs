fc157d9ae2a5b4d53f6beefc46303f22b7ab81f6
Oleh Ivsv <you@example.com>
2022-12-27 13:24:47 +0200

    Add basic ShoppingCartService

---
+++ src/ShoppingCart.Application/Errors/CartDoesNotExistsError.cs
@@ -0,0 +1,17 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Application.Errors
+{
+    public class CartDoesNotExistsError : Error
+    {
+        public CartDoesNotExistsError(int customerId)
+            :base($"The shopping cart does not exist for customer with id {customerId}")
+        {
+        }
+    }
+}
---
+++ src/ShoppingCart.Application/Services/IShoppingCartService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Application.Services
+{
+    public interface IShoppingCartService
+    {
+        Task<Result<Cart>> GetCartByCustomer(int customerId);
+    }
+}
