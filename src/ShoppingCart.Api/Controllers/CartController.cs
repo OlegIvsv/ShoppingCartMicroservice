6e14917350ae138ba757a0cf74ac8050a5571c8e
Oleh Ivsv <you@example.com>
2022-12-27 13:28:53 +0200

    Add CartController and endpoint which returns shopping cart by customer id

---
+++ src/ShoppingCart.Api/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using FluentResults;
+using MapsterMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Api.Contracts;
+using ShoppingCart.Application.Errors;
+using ShoppingCart.Application.Services;
+using ShoppingCart.Domain.Models;
+
+namespace ShoppingCart.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly IShoppingCartService _cartService;
+
+        public CartController(IShoppingCartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetShoppingCartByCustomerId(int customerId)
+        {
+            var cart = await _cartService.GetCartByCustomer(customerId);
+            if (cart.IsSuccess)
+            {
+                var responseCart = MapToResponse(cart.Value);
+                return Ok(responseCart);
+            }
+
+            return Problem(cart.Errors);
+        }
+
+        private IActionResult Problem(IList<IError> errors)
+        {
+            var firstError = errors.First();
+
+            var statusCode = firstError switch
+            {
+                CartDoesNotExistsError => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var problemResult = Problem(statusCode: statusCode, detail: firstError.Message);
+            return problemResult;
+        }
+
+        private CartResponse MapToResponse(Cart cart)
+        {
+            return new CartResponse(
+                cart.Id,
+                cart.CustomerId,
+                cart.Items.Select(
+                    item => new CartItemResponse(
+                        item.ProductId,
+                        item.UnitPrice,
+                        item.ProductTitle,
+                        item.Quantity)
+                    ).ToList()
+                );
+        }
+    }
+}
---
+++ src/ShoppingCart.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCart.Api.Mapping;
+
+namespace ShoppingCart.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+    }
+}
