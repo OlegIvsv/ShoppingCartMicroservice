39c5610c5dbe3e80a9374211f57c66ee43db45d7
Oleh Ivsv <you@example.com>
2022-12-28 13:59:14 +0200

    Add test for ShoppingCartService

--- tests/ShoppingCart.Application.Tests/ServicesTests/ShoppingCartServiceTests.cs
+++ tests/ShoppingCart.Application.Tests/ServicesTests/ShoppingCartServiceTests.cs
@@ -51,4 +51,42 @@ public class ShoppingCartServiceTests
         Assert.True(result.IsFailed);
         Assert.IsType<CartDoesNotExistsError>(result.Errors[0]);
     }
+
+    [Fact]
+    public async Task CreateCart_CartDoesNotExists_ReturnsCreatedCart()
+    {
+        //Arrange
+        int customerId = 1;
+        _repositoryMock
+            .Setup(repo => repo.Add(It.IsAny<Cart>()))
+            .ReturnsAsync(new Cart(customerId));
+        var service = new ShoppingCartService(_repositoryMock.Object);
+
+        //Act
+        var result = await service.CreateCart(customerId);
+
+        //Assert
+        Assert.NotNull(result.Value);
+        _repositoryMock.Verify(
+            repo => repo.Add(It.Is<Cart>(cart => cart.CustomerId == customerId)));
+    }
+
+    [Fact]
+    public async Task CreateCart_CartAlreadyExists_ReturnsError()
+    {
+        //Arrange
+        int customerId = 1;
+        _repositoryMock
+            .Setup(repo => repo.FindByCustomer(customerId))
+            .ReturnsAsync(new Cart(customerId));
+        var service = new ShoppingCartService(_repositoryMock.Object);
+
+        //Act
+        var result = await service.CreateCart(customerId);
+
+        //Assert
+        Assert.True(result.IsFailed);
+        _repositoryMock.Verify(
+            repo => repo.Add(It.IsAny<Cart>()), Times.Never());
+    }
 }
