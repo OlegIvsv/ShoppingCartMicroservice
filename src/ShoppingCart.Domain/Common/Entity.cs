1040979b9a778525719423ab000a948f810680d9
Oleh Ivsv <you@example.com>
2023-01-05 15:53:41 +0200

    Create base classes for domain model

---
+++ src/ShoppingCart.Domain/Common/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Domain.Common
+{
+    public abstract class Entity<T> : IEquatable<T> where T : Entity<T>
+    {
+        public virtual Guid Id { get; protected init; }
+
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity<T>);
+        }
+
+        public bool Equals(T? other)
+        {
+            if (other is null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+
+        public static bool operator ==(Entity<T> left, Entity<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<T> left, Entity<T> right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ src/ShoppingCart.Domain/Common/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Domain.Common
+{
+    public abstract class ValueObject<T> : IEquatable<T> where T : ValueObject<T>
+    {
+        public abstract IEnumerable<object> AtomicValuesList();
+
+        public bool Equals(T? other)
+        {
+            if (other is null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return AtomicValuesList().SequenceEqual(
+                other.AtomicValuesList());
+        }
+
+        public override bool Equals(object? obj)
+            => Equals(obj as T);
+
+        public override int GetHashCode()
+        {
+            return AtomicValuesList()
+                .Select(value => value != null ? value.GetHashCode() : 0)
+                .Aggregate((total, next) => total ^ next);
+        }
+
+        public static bool operator ==(ValueObject<T>? left, ValueObject<T>? right)
+            => Equals(left, right);
+
+        public static bool operator !=(ValueObject<T>? left, ValueObject<T>? right)
+            => !Equals(left, right);
+    }
+}
