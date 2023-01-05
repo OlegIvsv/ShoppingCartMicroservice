3ee5db6a8bd1d2c6d1ae66e6597b775324ef469b
Oleh Ivsv <you@example.com>
2023-01-05 18:55:24 +0200

    Update cart item response model

--- src/ShoppingCart.Api/Contracts/CartItemResponse.cs
+++ src/ShoppingCart.Api/Contracts/CartItemResponse.cs
@@ -3,8 +3,10 @@
 namespace ShoppingCart.Api.Contracts
 {
     public record CartItemResponse(
-        int ProductId, 
-        decimal UnitPrice,
-        string ProductTitle,
-        int Quantity);
+         Guid Id,
+         Guid ProductId,
+         decimal UnitPrice,
+         string ProductTitle,
+         int Quantity,
+         double Discount);
 }
