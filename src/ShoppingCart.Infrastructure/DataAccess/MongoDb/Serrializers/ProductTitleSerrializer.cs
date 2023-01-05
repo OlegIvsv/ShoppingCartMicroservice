57adc76e7b68c39d847183bc317d82ccce4f6eff
Oleh Ivsv <you@example.com>
2023-01-05 16:41:12 +0200

    Create a serializer for ProductTitle value object

---
+++ src/ShoppingCart.Infrastructure/DataAccess/MongoDb/Serrializers/ProductTitleSerrializer.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using ShoppingCart.Domain.ValueObjects;
+
+namespace ShoppingCart.Infrastructure.DataAccess.MongoDb.Serrializers
+{
+    public class ProductTitleSerrializer : SerializerBase<ProductTitle>
+    {
+        public override void Serialize(
+            BsonSerializationContext context,
+            BsonSerializationArgs args,
+            ProductTitle value)
+        {
+            context.Writer.WriteString(value.Value);
+        }
+
+        public override ProductTitle Deserialize(
+            BsonDeserializationContext context,
+            BsonDeserializationArgs args)
+        {
+            return ProductTitle.Create(context.Reader.ReadString()).Value;
+        }
+    }
+}
