e59bfecf1e8ab4bcbbf5addb5061cf88c40048c6
Oleh Ivsv <you@example.com>
2023-01-11 22:35:16 +0200

    Add classes for domain errors

---
+++ src/ShoppingCart.Domain/Errors/InvalidDiscountValueError.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+
+namespace ShoppingCart.Domain.Errors
+{
+    public class InvalidDiscountValueError : Error
+    {
+        public InvalidDiscountValueError(double value)
+            : base($"Invalid dicount value : {value}") { }
+    }
+}
+
