b2ab59cd3fafe4a2c91db3fcad37253832b873d1
Oleh Ivsv <you@example.com>
2023-01-17 18:45:26 +0200

    Add an attribute which is supposed to check if value is a valid guid id.

---
+++ src/ShoppingCart.Api/Contracts/ContractAttributes/GuidIdAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCart.Api.Contracts.ContractAttributes;
+
+[AttributeUsage(
+    AttributeTargets.Parameter |
+    AttributeTargets.Field | 
+    AttributeTargets.Property)]
+public class GuidIdAttribute : ValidationAttribute
+{
+    public GuidIdAttribute()
+        : base("The value is not a valid guid id")
+    {
+    }
+
+    public override bool IsValid(object? value)
+    {
+        if (value is not Guid guidValue)
+            return false;
+        if (guidValue == Guid.Empty)
+            return false;
+        return true;
+    }
+}
