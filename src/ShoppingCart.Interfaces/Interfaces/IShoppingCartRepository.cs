800804dd7cd173f1bd46edb5ab449d2d9cbca997
Oleh Ivsv <you@example.com>
2023-01-17 16:09:07 +0200

    Refactor ShoppingCart.Application. Adjust formatting and code style. Rename to ShoppingCart.Interfaces

---
+++ src/ShoppingCart.Interfaces/Interfaces/IShoppingCartRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingCart.Domain.Entities;
+
+namespace ShoppingCart.Interfaces.Interfaces;
+
+public interface IShoppingCartRepository
+{
+    Task Add(Cart cart);
+    Task<Cart?> FindByCustomer(Guid customerId);
+    Task<IEnumerable<Cart>> All();
+    Task<bool> Delete(Guid customerId);
+    Task Update(Cart cart);
+}
