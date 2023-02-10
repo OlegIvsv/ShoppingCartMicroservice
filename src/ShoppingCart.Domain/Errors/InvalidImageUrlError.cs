d63c9127352ee26e7bf2b9247e27fbee40e9f8c5
Oleh Ivsv <you@example.com>
2023-02-10 13:21:37 +0200

    Add a domain error for ImageUrl

---
+++ src/ShoppingCart.Domain/Errors/InvalidImageUrlError.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+
+namespace ShoppingCart.Domain.Errors;
+
+public class InvalidImageUrlError : Error
+{
+    public InvalidImageUrlError(string value)
+        : base($"'{value}' is not valid image url")
+    {
+    }
+}
