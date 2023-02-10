b9622915109bc2e998436b5bda9cd0fd6ae3614a
Oleh Ivsv <you@example.com>
2023-02-10 13:27:13 +0200

    Create a serializer for ImageUrl

---
+++ src/ShoppingCart.Infrastructure/DataAccess/MongoDb/Serializers/ImageUrlSerializer.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using ShoppingCart.Domain.ValueObjects;
+
+namespace ShoppingCart.Infrastructure.DataAccess.MongoDb.Serializers;
+
+internal class ImageUrlSerializer : SerializerBase<ImageUrl>
+{
+    public override void Serialize(
+        BsonSerializationContext context,
+        BsonSerializationArgs args,
+        ImageUrl value)
+    {
+        context.Writer.WriteString(value.Value);
+    }
+
+    public override ImageUrl Deserialize(
+        BsonDeserializationContext context,
+        BsonDeserializationArgs args)
+    {
+        return ImageUrl.Create(
+            BsonSerializer.Deserialize<string>(context.Reader)).Value;
+    }
+}
