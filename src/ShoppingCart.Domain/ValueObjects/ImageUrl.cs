42bdf365033410c9828e40fe49f881a740c5f661
Oleh Ivsv <you@example.com>
2023-02-10 13:19:53 +0200

    Add new ImageUrl value object

---
+++ src/ShoppingCart.Domain/ValueObjects/ImageUrl.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using ShoppingCart.Domain.Common;
+using ShoppingCart.Domain.Errors;
+
+namespace ShoppingCart.Domain.ValueObjects;
+
+public sealed class ImageUrl : ValueObject<ImageUrl>
+{
+    public string Value { get; }
+
+    private ImageUrl(string url)
+    {
+        Value = url;
+    }
+
+    public static Result<ImageUrl> Create(string url)
+    {
+        bool isValidFormat = Uri.TryCreate(url, UriKind.Absolute, out Uri? uriInfo);
+        bool isValidSchema = uriInfo.Scheme == Uri.UriSchemeHttp
+                             || uriInfo.Scheme == Uri.UriSchemeHttps;
+        if(isValidFormat && isValidSchema)
+            return new ImageUrl(url);
+        
+        return Result.Fail(new InvalidImageUrlError(url));
+    }
+
+    protected override IEnumerable<object> AtomicValuesList()
+    {
+        yield return Value;
+    }
+}
