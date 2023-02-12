f67a2556dcae5bee036b5a79578c64eea9d27fe6
Oleh Ivsv <you@example.com>
2023-02-12 18:31:36 +0200

    Update api contracts. Add the new properties according to the last changes in Cart domain model.

--- src/ShoppingCart.Api/Contracts/CartResponse.cs
+++ src/ShoppingCart.Api/Contracts/CartResponse.cs
@@ -2,12 +2,18 @@
 
 namespace ShoppingCart.Api.Contracts;
 
-public record CartResponse(Guid CustomerId, IEnumerable<CartItemResponse> Items)
+public record CartResponse(
+    Guid CustomerId,
+    bool IsAnonymous,
+    DateTime LastModifiedDate,
+    IEnumerable<CartItemResponse> Items)
 {
     public static CartResponse FromEntity(Cart cart)
     {
         return new CartResponse(
             cart.Id,
+            cart.IsAnonymous,
+            cart.LastModifiedDate,
             cart.Items.Select(CartItemResponse.FromEntity).ToArray());
     }
 }
