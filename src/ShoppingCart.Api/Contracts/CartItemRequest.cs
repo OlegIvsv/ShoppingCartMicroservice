dc8d49880048b21ce28ce392b25e6628c25df309
Oleh Ivsv <you@example.com>
2023-02-27 17:41:58 +0200

    Move CartItemRequest from CartItemBinder to Contracts and change accessibility to public

---
+++ src/ShoppingCart.Api/Contracts/CartItemRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Api.Contracts;
+
+public record CartItemRequest(
+    Guid ProductId,
+    decimal UnitPrice,
+    string ProductTitle,
+    int ItemQuantity,
+    double Discount,
+    string ImageUrl);
