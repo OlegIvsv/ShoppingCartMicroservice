6e21e9b8d13faed6a37b01687ee15a9337ba001c
Oleh Ivsv <you@example.com>
2023-03-07 14:59:01 +0200

    Add settings for deleting abandoned carts through DI

---
+++ src/ShoppingCart.Infrastructure/BackgroundJobs/CartsCleanUp/CartsCleanUpSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.Infrastructure.BackgroundJobs.CartsCleanUp;
+
+public class CartsCleanUpSettings
+{
+    public const string SectionName = "Jobs:CartCleanUpJob";
+    public TimeSpan CleanUpFrequency { get; set; }
+    public TimeSpan AbandonmentPeriod { get; set; }
+}
