52ceddf2901fa347ac44579414685f779bbd15f5
Oleh Ivsv <you@example.com>
2023-03-07 14:59:41 +0200

    Add an extension method for adding and configuring Quartz services

---
+++ src/ShoppingCart.Infrastructure/BackgroundJobs/JobExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Quartz;
+using ShoppingCart.Infrastructure.BackgroundJobs.CartsCleanUp;
+
+namespace ShoppingCart.Infrastructure.BackgroundJobs;
+
+public static class JobExtensions
+{
+    public static IServiceCollection AddQuartzJobs(
+        this IServiceCollection services,
+        ConfigurationManager configurationManager)
+    {
+        var cleanUpJobSettings = new CartsCleanUpSettings();
+        configurationManager.Bind(CartsCleanUpSettings.SectionName, cleanUpJobSettings);
+        services.AddSingleton(Options.Create(cleanUpJobSettings));
+
+        services.AddQuartz(quartz =>
+        {
+            quartz.UseInMemoryStore();
+            quartz.UseMicrosoftDependencyInjectionJobFactory();
+
+            var cleanerJobKey = CartsCleanUpJob.Key;
+            quartz.AddJob<CartsCleanUpJob>(opts =>
+                opts.WithIdentity(cleanerJobKey));
+
+            var triggerKey = new TriggerKey("carts-cleanup-trigger");
+            quartz.AddTrigger(configure => configure
+                .WithIdentity(triggerKey)
+                .WithSimpleSchedule(sched => sched
+                    .RepeatForever()
+                    .WithInterval(cleanUpJobSettings.CleanUpFrequency))
+                .ForJob(CartsCleanUpJob.Key)
+                .StartNow());
+        });
+
+        services.AddQuartzHostedService(config => config.WaitForJobsToComplete = true);
+
+        return services;
+    }
+}
