0bd73f68c80839a248d1c85ed4c7baf59e8f351c
Oleh Ivsv <you@example.com>
2023-03-15 12:45:37 +0200

    Upgrade db-manager-server with command line configuration
    Add command lines parameters for connection string, bd and collection. All of them are required.

--- tests/ShoppingCart.Api.Tests/ApiLoadTests/db-manager-server/main.py
+++ tests/ShoppingCart.Api.Tests/ApiLoadTests/db-manager-server/main.py
@@ -1,12 +1,34 @@
+# This script runs a single endpoint which refills the database with test data
+# PORT: 5000
+# COMMAND LINE PARAMETERS:
+# --connection - a connection string
+# --db - a name of database where data should be inserted
+# --collection - a name of collection where data should be inserted
+import sys
 from pymongo import MongoClient
 from random_models import RandomCart
 from flask import Flask, request, Response
+import argparse
 
-CONNECTION_STRING = "mongodb://adminn:passwordd@localhost:27017/"
-DB_NAME = "test_shop_db"
-COLLECTION_NAME = "test_collection_1"
+# 1. Read the params from command line #
+parser = argparse.ArgumentParser()
+parser.add_argument("--connection", type=str, help="Connection string")
+parser.add_argument("--db", type=str, help="Database name")
+parser.add_argument("--collection", type=str, help="Collection name")
+args = parser.parse_args()
 
+CONNECTION_STRING = args.connection
+DB_NAME = args.db
+COLLECTION_NAME = args.collection
+
+# 2. Check if all the necessary parameters were specified #
+if None in [CONNECTION_STRING, DB_NAME, COLLECTION_NAME]:
+    print("Error: Some parameters were not specified!", file=sys.stderr)
+    exit()
+
+# 3. Run the endpoint #
 app = Flask(__name__)
+
 
 def get_collection():
     client = MongoClient(CONNECTION_STRING, uuidRepresentation='csharpLegacy')
