eb9f69800b6212dc9c6715ba9b8598fc0d27f7aa
Oleh Ivsv <you@example.com>
2023-03-18 19:38:02 +0200

    Configure authorization/authentication services

--- src/ShoppingCart.Infrastructure/DependencyInjection.cs
+++ src/ShoppingCart.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ShoppingCart.Infrastructure.Auth;
 using ShoppingCart.Infrastructure.BackgroundJobs;
 using ShoppingCart.Infrastructure.DataAccess.MongoDb;
 using ShoppingCart.Interfaces.Interfaces;
@@ -21,7 +26,35 @@ public static class DependencyInjection
         services.AddSingleton(Options.Create(mongoSettings));
         /* Quartz and jobs */
         services.AddQuartzCleanUpJob(configurationManager);
-
+        
+        /* Authorization and authentication */
+        var authSettings = new AuthSettings();
+        configurationManager.Bind(AuthSettings.SectionName, authSettings);
+        services.AddSingleton(Options.Create(authSettings));
+        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidIssuer = authSettings.Issuer,
+                    ValidAudience = authSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authSettings.Key)),
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true
+                };
+            });
+        
+        services.AddAuthorization(config =>
+        {
+            config.AddPolicy("owner-only", policy =>
+                policy.Requirements.Add(new IsRegisteredRequirement()));
+        });
+        
+        services.AddSingleton<IAuthorizationHandler, AuthHandler>();
+        services.AddHttpContextAccessor();
         return services;
     }
 }
