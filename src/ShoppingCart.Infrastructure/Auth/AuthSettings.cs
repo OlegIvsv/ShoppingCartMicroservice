67274e0d9285032f937d1228a45ce2b477709846
Oleh Ivsv <you@example.com>
2023-03-18 19:34:09 +0200

    Add auth settings to receive authentication configuration

---
+++ src/ShoppingCart.Infrastructure/Auth/AuthSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingCart.Infrastructure.Auth;
+
+public class AuthSettings
+{
+    public const string SectionName = "Auth";
+    public const string IdClaimName = "registered-id";
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience {get; set; }
+}
