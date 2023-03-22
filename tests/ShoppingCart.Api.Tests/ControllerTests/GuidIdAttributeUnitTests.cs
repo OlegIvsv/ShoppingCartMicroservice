f163cafece363a8ce776e24896ab221f672ac0b3
Oleh Ivsv <you@example.com>
2023-03-22 13:26:58 +0200

    Add unit tests for GuidIdAttribute

---
+++ tests/ShoppingCart.Api.Tests/ControllerTests/GuidIdAttributeUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using ShoppingCart.Api.Contracts.ContractAttributes;
+using Xunit;
+
+namespace ShoppingCart.Api.Tests.ControllerTests;
+
+public class GuidIdAttributeTests
+{
+    [Theory]
+    [InlineData("invalid-guid")]
+    [InlineData(null)]
+    public void IsValid_ReturnsFalse_ForInvalidGuids(object value)
+    {
+        // Arrange
+        var attribute = new GuidIdAttribute();
+        // Act
+        var result = attribute.IsValid(value);
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValid_ReturnsFalse_ForEmptyGuid()
+    {
+        // Arrange
+        var attribute = new GuidIdAttribute();
+        var value = Guid.Empty;
+        // Act
+        var result = attribute.IsValid(value);
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValid_ReturnsTrue_ForValidGuid()
+    {
+        // Arrange
+        var attribute = new GuidIdAttribute();
+        var value = Guid.NewGuid();
+        // Act
+        var result = attribute.IsValid(value);
+        // Assert
+        Assert.True(result);
+    }
+}
