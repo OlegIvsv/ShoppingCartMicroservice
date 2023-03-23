31283db60138aea88a49b11453929e9e3e0820c3
Oleh Ivsv <you@example.com>
2023-03-23 17:51:29 +0200

    Remove the previous load tests, they are not needed anymore

--- tests/ShoppingCart.Api.Tests/LoadTests/common/command-line.js
+++
@@ -1,7 +0,0 @@
-
-export function readCommandLineParam(key){
-    if(key in __ENV)
-        return __ENV[key];
-    console.error(`Seems like the parameter '${key}' wasn't specified`);
-    throw new Error(`Not all params were specified`);
-}
