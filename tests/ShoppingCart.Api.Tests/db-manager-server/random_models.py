359b25ace01f5280123edde26cf3fde44110b9f0
Oleh Ivsv <you@example.com>
2023-03-23 17:47:02 +0200

    Craete an environment for integration tests to run in docker containers with Grafana visualization

---
+++ tests/ShoppingCart.Api.Tests/db-manager-server/random_models.py
@@ -0,0 +1,41 @@
+import uuid
+from data_generators import random_title, random_image
+from bson import Decimal128 as MongoDecimal
+import random
+
+
+class RandomCartItem:
+
+    def __init__(self, max_price, max_quantity):
+        self.title = random_title()
+        self.price = MongoDecimal(str(random.uniform(0.1, max_price)))
+        self.quantity = random.randint(1, max_quantity)
+        self.discount = random.uniform(0, 0.7)
+        self.id = uuid.uuid4()
+        self.product_id = uuid.uuid4()
+        self.imageUrl = random_image()
+
+    def to_mongo_document(self) -> dict:
+        return {
+            "_id": self.id,
+            "productId": self.product_id,
+            "productTitle": self.title,
+            "unitPrice": self.price,
+            "quantity": self.quantity,
+            "discount": round(self.discount, 2),
+            "imageUrl": self.imageUrl
+        }
+
+
+class RandomCart:
+
+    def __init__(self, max_items_number):
+        self.items = [RandomCartItem(1000, 20) for i in range(1, max_items_number)]
+        self.id = uuid.uuid4()
+
+    def to_mongo_document(self) -> dict:
+         return {
+            "_id": self.id,
+            "items": ([item.to_mongo_document() for item in self.items])
+        }
+
